e4754a92f92b404dbb01561dde9f7f56d24133db
braziel <braziel@discente.ufg.br>
2024-12-26 09:13:16 -0300

    Entity Order, part 1

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,5 +1,7 @@
 package com.example.course.entities;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import java.io.Serializable;
@@ -10,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 @Table(name = "tb_user")
@@ -24,6 +27,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -73,6 +79,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders(){
+        return orders;
     }
 
     @Override
