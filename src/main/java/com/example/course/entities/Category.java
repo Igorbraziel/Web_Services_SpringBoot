6fd427ee1886b6ca4cd3ba210bec35b6c665336f
braziel <braziel@discente.ufg.br>
2024-12-30 09:38:53 -0300

    Creating Product Entity

--- src/main/java/com/example/course/entities/Category.java
+++ src/main/java/com/example/course/entities/Category.java
@@ -2,10 +2,12 @@ package com.example.course.entities;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
 
 import java.io.Serializable;
 import java.io.Serial;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -18,6 +20,9 @@ public class Category implements Serializable {
     private Long id;
 
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category(){}
 
@@ -40,6 +45,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts(){
+        return products;
     }
 
     @Override
