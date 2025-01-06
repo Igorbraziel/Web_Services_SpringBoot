abbc1e2c8188ee16d36977ebbcaa082701896194
braziel <braziel@discente.ufg.br>
2025-01-06 09:47:06 -0300

    Exception Handling - update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -4,6 +4,7 @@ package com.example.course.services;
 import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User updatedUser){
-        User currentUser = repository.getReferenceById(id);
-        updateData(currentUser, updatedUser);
-        return repository.save(currentUser);
+        try {
+            User currentUser = repository.getReferenceById(id);
+            updateData(currentUser, updatedUser);
+            return repository.save(currentUser);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User currentUser, User updatedUser){
